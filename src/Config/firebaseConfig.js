edf9f81f9f7ad502b8a91ba7e746852609f79ec2
vivek ranjan <41664287+Viwekrajj@users.noreply.github.com>
2020-12-29 01:19:45 +0530

    Add files via upload

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyD6NZPN-jdWSCUwPaL2Prt1sl9c9qrEqbg",
+    authDomain: "gitapi-2dfe3.firebaseapp.com",
+    databaseURL: "https://gitapi-2dfe3.firebaseio.com",
+    projectId: "gitapi-2dfe3",
+    storageBucket: "gitapi-2dfe3.appspot.com",
+    messagingSenderId: "523637581776",
+    appId: "1:523637581776:web:245c4e1dee54d0dd5ffbd5",
+    measurementId: "G-Q0EDQBTXJX"
+  };
